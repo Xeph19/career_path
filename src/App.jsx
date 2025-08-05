3f56d383c738576211f45ed4d99ccc75025a0ea6
KeirtWilliam Gemperle <keirtwilliamgemperle@gmail.com>
2025-08-05 21:28:16 +0800

    setting up MUI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,17 @@
-import CareerCard from "./components/CareerCard";
-import SigninForm from "./components/SigninForm";
-const careers = [
-  {
-    title: "Software Development",
-    description:
-      "Design, develop, and maintain software applications across various platforms and industries.",
-    tags: ["JavaScript", "Python", "Java"],
-  },
-  {
-    title: "Cybersecurity",
-    description:
-      "Protect systems, networks, and data from digital attacks and unauthorized access.",
-    tags: ["Ethical Hacking", "Network Security"],
-  },
-  {
-    title: "Data Science",
-    description:
-      "Extract insights from complex data sets to drive business decisions and strategy.",
-    tags: ["Python", "SQL"],
-  },
-  {
-    title: "Cloud Computing",
-    description:"Design, implement, and manage cloud-based solutions for businesses and organizations.",
-    tags: ["AWS","Azure"],
-  },
-  {
-    title: "AI/ML Engineering",
-    description: "Build intelligent systems and algorithms that can learn from and make decisions based on data.",
-    tags: ["Python","TensorFlow"],
-  },
-  {
-    title:"UI/UX Design",
-    description: "Create intuitive and visually appealing user interfaces and experiences for digital products.",
-    tags: ["Figma","Adobe XD"]
-  }
-];
-
-
-
+import LandingPage from './landingpage';
+import picture1 from './images/picture1.png';
+import ResponsiveAppBar from './navbar';
+import BasicButtons from './components/Button';
 
 function App() {
   return (
-    <>
-    <div className="career-list">
-      {careers.map((career, index) => (
-        <CareerCard
-          key={index}
-          title={career.title}
-          description={career.description}
-          tags={career.tags}
-        />
-      ))}
+    <div>
+     <ResponsiveAppBar />
+      <LandingPage />
+      <BasicButtons />
+      <img src={picture1} className="right-side" alt="pictures" />
     </div>
-    <SigninForm
-    title="Welcome to Dalan"
-     para="If You’re Lost in the Code, We’ll Help You Find the Road"
-     btn="LOGIN"
-    text="Don’t have an account yet?"
-    Fields={[
-    { label: "Email", name: "email", type: "email" },
-    { label: "Password", name: "password", type: "password" },
-  ]}
-/>
-  <SigninForm 
-   title="Create New Account"
-   para="If You’re Lost in the Code, We’ll Help You Find the Road"
-   btn="Sign Up"
-   text="Already have an account?"
-   Fields={[
-    { label: "Full Name", name: "name", type: "text" },
-    { label: "Email", name: "email", type: "email" },
-    { label: "Password", name: "password", type: "password" }
-   ]}
-  />
-    </>
   );
-  
 }
 
 export default App;
---
+++ src/landingpage.jsx
@@ -0,0 +1,11 @@
+function LandingPage(){
+    return(
+
+        <>
+        <h1>Find Your Perfect <span className="blue-clr">IT Career Path</span></h1>
+        <p className="landingpage-text">Discover which technology career aligns with your skills, interests, and personality. Get personalized recommendations and roadmap to success.</p>
+        
+        </>
+    );
+}
+export default LandingPage;
