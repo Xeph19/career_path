bd19c35e8717edfc37bcc3d653062f1751f54c44
KeirtWilliam Gemperle <keirtwilliamgemperle@gmail.com>
2025-08-05 21:34:59 +0800

    third commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
+import LandingPage from './landingpage';
+import picture1 from './images/picture1.png';
+import ResponsiveAppBar from './navbar';
+import BasicButtons from './components/Button';
 
 function App() {
   return (
-    <>
-  
-    </>
+    <div>
+     <ResponsiveAppBar />
+      <LandingPage />
+      <BasicButtons />
+      <img src={picture1} className="right-side" alt="pictures" />
+    </div>
   );
-  
 }
-export default App;
 
+export default App;
